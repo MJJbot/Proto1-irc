3e60f0b8e89a80539c6c47c0b33dd828f6d8e22b
5vermind <39900428+5vermind@users.noreply.github.com>
2019-10-09 22:46:08 +0900

    Update bot_instance.py
    oauth 올려놓는건 좀 그래서 일단 지워놓음

--- flask/bot_instance.py
+++ flask/bot_instance.py
@@ -8,7 +8,7 @@ class bot_instance:
     def __init__(self, channel):
         self.chat = twitch.Chat(channel=channel,
                        nickname='bot',
-                       oauth='oauth:la6kj4pn3vzfdvvkfrg8y5eqvbuxjk',
+                       oauth='oauth:///',
                        helix=twitch.Helix(client_id='hqiwd4o8a3mf6l7t7l0xge8qt7ksob', use_cache=True))
         self.chat.subscribe(handle_message)
 
