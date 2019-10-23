9bcd641994a07b0f0bf630a2456a694baf8a203c
5vermind <5vermind@gmail.com>
2019-10-23 16:34:13 +0900

    크롤링

--- bot_instance.py
+++ bot_instance.py
@@ -3,14 +3,15 @@ import logging
 import twitch.helix as helix
 import twitch.chat as chat
 import webhook
+import time
 
 class bot_instance:
-    def __init__(self, channel):
+    def __init__(self, channel):#api:webhook.api):
         self.channel = channel
-        api = webhook.api()
-        self.id = api.user_id(channel, 'la6kj4pn3vzfdvvkfrg8y5eqvbuxjk') #la6kj4pn3vzfdvvkfrg8y5eqvbuxjk   qxhcdwsgg5m3qm7or17f3lo51soznk
-        api.sub_streamchange(id, 'hqiwd4o8a3mf6l7t7l0xge8qt7ksob')
-        
+        # self.api = api
+        # self._id = self.api.user_id(channel) #la6kj4pn3vzfdvvkfrg8y5eqvbuxjk   qxhcdwsgg5m3qm7or17f3lo51soznk
+        # self.api.sub_streamchange(self._id)
+
         self.helix=twitch.Helix(client_id='hqiwd4o8a3mf6l7t7l0xge8qt7ksob', use_cache=True)
         self.chat = twitch.Chat(channel=channel,
                        nickname='bot',
@@ -20,13 +21,11 @@ class bot_instance:
             
     def handle_message(self, message: twitch.chat.Message) -> None:
         self.buf.append(message.sender + ":" + message.text + "\n")
-        print(message.sender + ":" + message.text + "\n")
 
     def stop_bot(self):
-        file = open(self.channel+".txt", "w", encoding='utf-8')
+        file = open(self.channel+ time.strftime('%Y-%m-%d', time.localtime(time.time())) +".txt", "w", encoding='utf-8')
         file.write(''.join(self.buf))
         self.buf.clear()
-        self.chat.dispose()
 
     def start_bot(self):
         self.chat.subscribe(self.handle_message)
