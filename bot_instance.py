a80f26f2969b0532632a302c997d9f0876e8b86c
5vermind <39900428+5vermind@users.noreply.github.com>
2019-10-27 00:48:02 +0900

    Update bot_instance.py

--- bot_instance.py
+++ bot_instance.py
@@ -1,8 +1,8 @@
 import twitch
 
 def handle_message(message: twitch.chat.Message) -> None:
-    if message.text.startswith("!하이"):
-        message.chat.send("하이요")
+    if message.text.startswith("!"):
+        return
 
 class bot_instance:
     def __init__(self, channel):
@@ -11,3 +11,4 @@ class bot_instance:
                        oauth='oauth:la6kj4pn3vzfdvvkfrg8y5eqvbuxjk',
                        helix=twitch.Helix(client_id='hqiwd4o8a3mf6l7t7l0xge8qt7ksob', use_cache=True))
         self.chat.subscribe(handle_message)
+    
